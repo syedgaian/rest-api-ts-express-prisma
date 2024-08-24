dba4cca96e0ca1079b5d2bc4c1f9abe09e580c4e
syedgaian <salman.s@mobiusdtaas.ai>
2024-08-24 22:49:57 +0530

    added creat assisatant impl in controller

---
+++ src/features/assistants/presentation/controller.ts
@@ -0,0 +1,47 @@
+// src\features\todos\presentation\controller.ts
+
+import { type NextFunction, type Request, type Response } from 'express';
+
+import { type SuccessResponse, HttpCode, ONE, TEN } from '../../../core';
+import { PaginationDto, type PaginationResponseEntity } from '../../shared';
+
+import {
+    CreateAssistant,
+    CreateAssistantDto,
+    type AssistantEntity,
+    type AssistantRepository
+} from '../domain';
+
+interface Params {
+    id: string;
+}
+
+interface RequestBody {
+    name: string;
+    version: string;
+}
+
+interface RequestQuery {
+    page: string;
+    limit: string;
+}
+
+export class AssistantController {
+    //* Dependency injection
+    constructor(private readonly repository: AssistantRepository) { }
+
+
+    public create = (
+        req: Request<unknown, unknown, RequestBody>,
+        res: Response<SuccessResponse<AssistantEntity>>,
+        next: NextFunction
+    ): void => {
+        const { name, version } = req.body;
+        const createDto = CreateAssistantDto.create({ name, version });
+        new CreateAssistant(this.repository)
+            .execute(createDto)
+            .then((result) => res.status(HttpCode.CREATED).json({ data: result }))
+            .catch(next);
+    };
+
+}
