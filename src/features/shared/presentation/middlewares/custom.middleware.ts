765db6f2a4760963ace072078ba2a3e2ee8a3bda
syedgaian <salman.s@mobiusdtaas.ai>
2024-08-27 11:33:29 +0530

    added logger to log to files and console

--- src/features/shared/presentation/middlewares/custom.middleware.ts
+++ src/features/shared/presentation/middlewares/custom.middleware.ts
@@ -1,11 +1,25 @@
 import { type Response, type NextFunction, type Request } from 'express';
+import rfs from 'rotating-file-stream';
+import path from "path";
+import morgan from "morgan";
 
 export class CustomMiddlewares {
-	//* Dependency injection
-	// constructor() {}
+    //* Dependency injection
+    // constructor() {}
 
-	public static writeInConsole = (_req: Request, _res: Response, next: NextFunction): void => {
-		// console.log('Hello from the Middleware');
-		next();
-	};
+    public static writeInConsole = (_req: Request, _res: Response, next: NextFunction): void => {
+        // console.log('Hello from the Middleware');
+        next();
+    };
+
+    private static accessLogStream = rfs.createStream('access.log', {
+        size: "10M",
+        interval: '1d', // rotate daily,
+        compress: "gzip",
+        path: path.join(process.cwd(), 'log')
+    })
+
+    public static logToFile = morgan('combined', { stream: this.accessLogStream });
+
+    public static logToConsole = morgan('combined');
 }
