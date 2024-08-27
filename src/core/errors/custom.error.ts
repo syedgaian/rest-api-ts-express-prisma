b517e527f30fb783b2d4dacca323d78a7ec3e261
syedgaian <salman.s@mobiusdtaas.ai>
2024-08-27 18:12:41 +0530

    added a middleware for methode not allowed error

--- src/core/errors/custom.error.ts
+++ src/core/errors/custom.error.ts
@@ -4,47 +4,51 @@ import { HttpCode } from '../constants';
 import { type ValidationType } from '../types';
 
 interface AppErrorArgs {
-	name?: string;
-	statusCode: HttpCode;
-	message: string;
-	isOperational?: boolean;
-	validationErrors?: ValidationType[];
+    name?: string;
+    statusCode: HttpCode;
+    message: string;
+    isOperational?: boolean;
+    validationErrors?: ValidationType[];
 }
 
 export class AppError extends Error {
-	public readonly name: string;
-	public readonly statusCode: HttpCode;
-	public readonly isOperational: boolean = true;
-	public readonly validationErrors?: ValidationType[];
+    public readonly name: string;
+    public readonly statusCode: HttpCode;
+    public readonly isOperational: boolean = true;
+    public readonly validationErrors?: ValidationType[];
 
-	constructor(args: AppErrorArgs) {
-		const { message, name, statusCode, isOperational, validationErrors } = args;
-		super(message);
-		Object.setPrototypeOf(this, new.target.prototype);
-		this.name = name ?? 'Aplication Error';
-		this.statusCode = statusCode;
-		if (isOperational !== undefined) this.isOperational = isOperational;
-		this.validationErrors = validationErrors;
-		Error.captureStackTrace(this);
-	}
+    constructor(args: AppErrorArgs) {
+        const { message, name, statusCode, isOperational, validationErrors } = args;
+        super(message);
+        Object.setPrototypeOf(this, new.target.prototype);
+        this.name = name ?? 'Aplication Error';
+        this.statusCode = statusCode;
+        if (isOperational !== undefined) this.isOperational = isOperational;
+        this.validationErrors = validationErrors;
+        Error.captureStackTrace(this);
+    }
 
-	static badRequest(message: string, validationErrors?: ValidationType[]): AppError {
-		return new AppError({ name: 'BadRequestError', message, statusCode: HttpCode.BAD_REQUEST, validationErrors });
-	}
+    static badRequest(message: string, validationErrors?: ValidationType[]): AppError {
+        return new AppError({ name: 'BadRequestError', message, statusCode: HttpCode.BAD_REQUEST, validationErrors });
+    }
 
-	static unauthorized(message: string): AppError {
-		return new AppError({ name: 'UnauthorizedError', message, statusCode: HttpCode.UNAUTHORIZED });
-	}
+    static unauthorized(message: string): AppError {
+        return new AppError({ name: 'UnauthorizedError', message, statusCode: HttpCode.UNAUTHORIZED });
+    }
 
-	static forbidden(message: string): AppError {
-		return new AppError({ name: 'ForbiddenError', message, statusCode: HttpCode.FORBIDDEN });
-	}
+    static forbidden(message: string): AppError {
+        return new AppError({ name: 'ForbiddenError', message, statusCode: HttpCode.FORBIDDEN });
+    }
 
-	static notFound(message: string): AppError {
-		return new AppError({ name: 'NotFoundError', message, statusCode: HttpCode.NOT_FOUND });
-	}
+    static notFound(message: string): AppError {
+        return new AppError({ name: 'NotFoundError', message, statusCode: HttpCode.NOT_FOUND });
+    }
 
-	static internalServer(message: string): AppError {
-		return new AppError({ name: 'InternalServerError', message, statusCode: HttpCode.INTERNAL_SERVER_ERROR });
-	}
+    static internalServer(message: string): AppError {
+        return new AppError({ name: 'InternalServerError', message, statusCode: HttpCode.INTERNAL_SERVER_ERROR });
+    }
+
+    static methodNotAllowed(message: string): AppError {
+        return new AppError({ name: 'MethodNotAllowed', message, statusCode: HttpCode.METHOD_NOT_ALLOWED })
+    }
 }
